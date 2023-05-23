c791751b4c00faf69937e5ae110f1b3fb775d9c0
Krishna Kumar <nnkrishna714@gmail.com>
2023-05-23 12:05:18 +0530

    commit

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # WD_12_python-16-5-2023-
 This is python new batch..
+
+https://www.w3resource.com/python-exercises/
+
+variable 
+comment
+datatypes
+> integer --> int
+> float  
+> string  --> str
+> list  
+> tuple
+> set
+> dictionary --> dict
+> frozenset
+> boolean  --> bool
+> complex 
+
+if else
+nested if else
+if elif else
+nested if elif else
+
+for loop 
+nested for loop 
+while loop 
+nested while loop 
+
+function 
+lambda function annonymous function 
---
+++ variable.py
@@ -0,0 +1,19 @@
+# Variables:
+# Rule1: A variable name never start with a number
+# Rule2: A variable never contain special character except underscore(_)
+# Rule3: A variable name must be readable.
+
+#1a=10
+#print(1a)  # it is an error beacause variable name start with a number
+print("Hello world")
+#!"#$%&'()*+,-./:;<=>?@[\]^`{|}~
+# krishna1$=10
+# print(krishna1$)
+
+krishna_prajapati=10   # snake case
+krishnaPrajapati=10  # camel case
+KrishnaPrajapati=10 # Pascal case
+_=20
+print(_)
+a=10
+
